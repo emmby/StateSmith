7bea877b6456bce4b8c6d7cccaf2f78a81c2f271
Mike Burton <mb-github@niskala.org>
2025-06-20 09:51:30 -0700

    make TestHelper a little more generalized

--- src/StateSmithTest/Antlr/Antlr4Test.cs
+++ src/StateSmithTest/Antlr/Antlr4Test.cs
@@ -5,6 +5,8 @@ using StateSmith.Input.Antlr4;
 using StateSmith.Output;
 using StateSmith.Runner;
 using StateSmith.SmGraph;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 
 // todolow look into this: https://www.antlr.org/api/Java/org/antlr/v4/runtime/TokenStreamRewriter.html
 
@@ -418,7 +420,8 @@ public class Antlr4Test : CommonTestHelper
     [Fact]
     public void StateMachineWithBehaviorsIntegrationTest()
     {
-        InputSmBuilder inputSmBuilder = TestHelper.CreateInputSmBuilder();
+        IServiceProvider serviceProvider = TestHelper.CreateServiceProvider();
+        InputSmBuilder inputSmBuilder = serviceProvider.GetRequiredService<InputSmBuilder>();
         inputSmBuilder.ConvertDiagramFileToSmVertices(TestHelper.GetThisDir() + "/" + "Antlr4Tests.drawio");
         inputSmBuilder.FindStateMachineByName("MySm1");
         inputSmBuilder.FinishRunning();
